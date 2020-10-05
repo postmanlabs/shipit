1563a76e74898a4c7dc0dc57283712789137a0b8
Udit Vasu <udit@pm.me>
2020-10-05 14:07:21 +0530

    Add support for devDependencies in validate dependencies task

--- tasks/validateDependencies.js
+++ tasks/validateDependencies.js
@@ -3,15 +3,16 @@ module.exports = async function validateDependencies (exec, console, { dependenc
 
   console.warn('Making sure that dependencies are updated...')
 
-  const { dependencies } = require(`${process.cwd()}/package.json`)
+  const { dependencies, devDependencies } = require(`${process.cwd()}/package.json`)
+  const installedDependencies = { ...devDependencies, ...dependencies }
 
   for (const dependency of dependencyList) {
-    if (!(dependency in dependencies)) {
+    if (!(dependency in installedDependencies)) {
       throw new Error(`Dependency "${dependency}" is not installed.`)
     }
 
     const latestVersion = (await exec(`npm show ${dependency} version`)).trim()
-    const installedVersion = dependencies[dependency]
+    const installedVersion = installedDependencies[dependency]
 
     if (latestVersion !== installedVersion) {
       throw new Error(`Expected "${dependency}" to be v${latestVersion} but found v${installedVersion}.`)
